4821211b36fb57880baffa95352859de39a5d744
MonifD <dourimoif@gmail.com>
2026-02-24 15:03:45 +0100

    modify config EsLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,36 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.js"],
+    ...js.configs.recommended,
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn", // ne bloque plus le pipeline
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    files: ["tests/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        jest: "readonly",
+      },
+    },
+  },
 ]);
